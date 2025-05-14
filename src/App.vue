5d25842d497848c2ec81a6b5045f97f3c7342eab
Md. Mahmudul Hasan <antormahmud789@gmail.com>
2025-05-14 14:56:39 +0600

    Start Todo List App project Connect to git

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, onMounted, computed, watch } from "vue";
+
+const todos = ref([]);
+const name = ref("");
+
+const input_content = ref("");
+const input_category = ref(null);
+
+const todos_asc = computed(() =>
+  todos.value.sort((a, b) => {
+    return b.createdAt - a.createdAt;
+  })
+);
+</script>
+
+<template>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input type="text" placeholder="name Here" v-model="name" />
+      </h2>
+    </section>
+  </main>
+</template>
